9947b0997807ae1c007d0086bf79cfdff5af0ea0
Diego Garcia <qtimpot@gmail.com>
2022-08-14 20:15:32 -0400

    Add template code

---
+++ template/src/index.ts
@@ -0,0 +1,48 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+let scene: THREE.Scene;
+let camera: THREE.PerspectiveCamera;
+let renderer: THREE.Renderer;
+
+init();
+
+function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(0, 3, 10);
+
+  const ambient = new THREE.HemisphereLight(0xffffbb, 0x080820);
+  scene.add(ambient);
+
+  const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+  light.position.set(1, 10, 6);
+  scene.add(light);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+
+  const controls = new OrbitControls(camera, renderer.domElement);
+  controls.target.set(0, 4, 0);
+  controls.update();
+
+  // Add meshes here
+
+
+
+  window.addEventListener('resize', resize, false);
+
+  update();
+}
+
+function update() {
+  requestAnimationFrame(update);
+  renderer.render(scene, camera);
+}
+
+function resize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
